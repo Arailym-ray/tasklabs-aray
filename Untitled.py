2490b8a88e6be2ba16ad8c1aceb5fd6f65cc1cef
Arailym Tleubayeva <65153880+Arailym-ray@users.noreply.github.com>
2021-02-16 12:32:23 +0600

    Задание 1

---
+++ Untitled.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import math
+a=3
+b=4
+y= math.sqrt(a**2+b**2)
+p=y+a+b
+print('p=',p)
+
+
+# In[2]:
+
+
+import math
+a=0
+b=3
+y= math.sqrt(a**2+b**2)
+p=y+a+b
+print('p=',p)
+
+
+# In[ ]:
+
+
+
+
